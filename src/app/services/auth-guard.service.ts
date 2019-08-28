9dc1dbc1b2824ea02b90cfbe80c2354a779a3d79
李文浩 <yuanyuanliwenhao@163.com>
2019-08-28 10:43:16 +0800

    调整语法

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -5,42 +5,44 @@ import {Router} from '@angular/router';
 import {Store} from '@ngrx/store';
 import * as fromRoot from '../reducers';
 import {Observable} from 'rxjs';
+import {defaultIfEmpty, map} from 'rxjs/operators';
 
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-    /**
-     * 构造函数用于注入服务的依赖以及进行必要的初始化
-     *
-     * @param router 路由注入，用于导航处理
-     * @param store$ redux store注入，用于状态管理
-     */
-    constructor(private store$: Store<fromRoot.State>, private router: Router) {
-    }
+  /**
+   * 构造函数用于注入服务的依赖以及进行必要的初始化
+   *
+   * @param router 路由注入，用于导航处理
+   * @param store$ redux store注入，用于状态管理
+   */
+  constructor(private store$: Store<fromRoot.State>, private router: Router) {
+  }
 
-    /**
-     * 用于判断是否可以激活该路由
-     *
-     * @param route
-     */
-    canActivate(route: ActivatedRouteSnapshot,
-                state: RouterStateSnapshot): Observable<boolean> | boolean {
-        return this.checkAuth();
-    }
+  /**
+   * 用于判断是否可以激活该路由
+   *
+   * @param route
+   */
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | boolean {
+    return this.checkAuth();
+  }
 
-    checkAuth(): Observable<boolean> {
-        return this.store$
-            .select(s => s.auth)
-            .map(auth => {
-                console.log(auth);
-                const result = auth.token !== undefined && auth.token !== null;
-                if (!result) {
-                    this.router.navigate(['/login']);
-                    // this.store$.dispatch(go(['/login']));
-                }
-                return result;
-            })
-            .defaultIfEmpty(false);
-    }
+  checkAuth(): Observable<boolean> {
+    return this.store$
+      .select(s => s.auth).pipe(
+        map(auth => {
+          console.log(auth);
+          const result = auth.token !== undefined && auth.token !== null;
+          if (!result) {
+            this.router.navigate(['/login']);
+            // this.store$.dispatch(go(['/login']));
+          }
+          return result;
+        }),
+        defaultIfEmpty(false)
+      );
+  }
 }
