a06b24d1eaa083a9d2ccca8d6f49fa2e18b7f552
李文浩 <yuanyuanliwenhao@163.com>
2019-09-25 17:25:47 +0800

    添加注释

--- src/app/effects/quote.effects.ts
+++ src/app/effects/quote.effects.ts
@@ -10,12 +10,12 @@ import {catchError, map, switchMap} from 'rxjs/operators';
 @Injectable()
 export class QuoteEffects {
     /**
-     *
+     * 用 @Effect() 修饰器来标明这是一个 Effect
      */
     @Effect()
-    quote$: Observable<Action> = this.actions$.pipe(
-      ofType(actions.ActionTypes.QUOTE_LOAD),
-      map((action: actions.QuoteLoadAction) => action.payload),
+    quote$: Observable<Action> = this.actions$.pipe( // action 信号流
+      ofType(actions.ActionTypes.QUOTE_LOAD), // 如果是 QUOTE_LOAD
+      map((action: actions.QuoteLoadAction) => action.payload), // 转换成 action 的 payload 数据流
       switchMap(() => this.quoteService.getQuote().pipe(
           map(quote => new actions.QuoteLoadSuccessAction(quote)),
           catchError(err => of(new actions.QuoteLoadFailAction(JSON.stringify(err))))
@@ -23,6 +23,9 @@ export class QuoteEffects {
       )
     );
 
+    /**
+     * 通过构造注入需要的服务和 action 信号流
+     */
     constructor(private actions$: Actions, private quoteService: QuoteService) {
     }
 }
